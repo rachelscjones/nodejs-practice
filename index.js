3f0d8670ea34a1904eca0de32b25005ad0e22cef
Rachel Jones <rachelscjones@gmail.com>
2023-08-22 12:28:22 +0100

    set up second route

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const app = express();
 
-// anonymous function taking 2 arguments (req = request; res = response)
+// [anonymous function taking 2 arguments (req = request; res = response)]
+// First route
 app.get('/', (req,res) => {
   res.send('Hello world!')
 });
 
-app.listen(3000, function(){
+// Second route
+let animals = ['Cat', 'Elephant', 'Polar Bear', 'Seahorse']
+
+app.get('/animals/:id', (req,res) => {
+  let id = req.params.id
+  res.send('Your lucky animal is ' + animals[id])
+});
+
+// Server set up to listen on a specific port
+app.listen(3000, () => {
   console.log('App server running on port 3000')
-})
+});
